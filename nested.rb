1300fc168fa9389d4f8bd9a48c0d7507555cdea4
Jonathan Norman <jnorman2123@gmail.com>
2019-09-30 20:55:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,24 @@ def dennis_ritchies_language
   programmer_hash[:dennis_ritchie][:languages][0]
 end 
 
-
+def adding_matz
+  programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:matz] = {}
+end 
 
 
 
