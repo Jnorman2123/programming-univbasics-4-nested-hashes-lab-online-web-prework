2d59f85f5e314be84c3543b4ab18db5d3b38da6a
Jonathan Norman <jnorman2123@gmail.com>
2019-09-30 20:41:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -7,5 +7,9 @@ def hopper
 end 
 
 def alan_kay_is_known_for 
-  
+  programmer_hash = {
+  grace_hopper: {known_for: "COBOL", languages: ["COBOL", "FORTRAN"]},
+  alan_kay: {known_for: "Object Orientation", languages: []}
+  }
+  programmer_hash[:alan_kay][:known_for]
 end 
